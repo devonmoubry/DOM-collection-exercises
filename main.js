e6197450cc9b791d40a2af0c9369bb30f138b244
Devon Moubry <devon@moubry.com>
2017-03-14 14:33:52 -0500

    javascript comments, html content, css style

---
+++ main.js
@@ -0,0 +1,18 @@
+/****************Farmers Market****************/
+//access each farmers-market object
+
+//output to DOM
+
+/****************Giphy Cats****************/
+
+//figure out which property is a thumbnail size
+
+//access object
+
+//access array
+
+//access object
+
+//access thumbnail property
+
+//output thumbnail img to DOM
